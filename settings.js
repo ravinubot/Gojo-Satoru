e48cff83bda6bf2499bc51be80d553fc0d8385b5
ravinubot <112113894+ravinubot@users.noreply.github.com>
2022-08-27 15:49:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94717988266']
+global.premium = ['94717988266']
+global.ownernomer = '94717988266'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
